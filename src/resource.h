44364a53f4e3b3881221508b6a7053fed67decc3
Kevin <anyboo@qq.com>
2020-03-20 19:34:51 +0800

    Update Version Resource

--- src/resource.h
+++ src/resource.h
@@ -4,6 +4,25 @@
 
 // 新对象的下一组默认值
 // 
+#define STRINGIZE2(s)		#s
+#define STRINGIZE(s)        STRINGIZE2(s)
+
+#define MAIN				1 
+#define Major				0 
+#define Minor				0 
+#define FileIndex			1
+
+#define VTSFILEVERSION			MAIN,Major,Minor,FileIndex
+#define VTSFILEVERSION_STR		STRINGIZE(MAIN) "." STRINGIZE(Major) "." STRINGIZE(Minor) "." STRINGIZE(FileIndex)
+
+#define WMAIN				2 
+#define WMajor				0 
+#define WMinor				6 
+#define WFileIndex			79
+
+#define VTSPRODUCTVERSION			WMAIN,WMajor,WMinor,WFileIndex
+#define VTSPRODUCTVERSION_STR		STRINGIZE(WMAIN) "." STRINGIZE(WMajor) "." STRINGIZE(WMinor) "." STRINGIZE(WFileIndex)
+
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
